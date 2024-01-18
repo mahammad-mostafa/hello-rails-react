c726b6d7504a5319123d496bb9612cb807037a0a
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-18 02:24:37 +0200

    Imported basic react components & rendered them

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+  return (<h1>Hello World!</h1>);
+}
+createRoot(document.getElementById('root')).render(<App />);
