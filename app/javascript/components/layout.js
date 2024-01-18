a32d6b3e61ced6fe5daba2bb4f52a559e9eb487d
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-18 05:17:34 +0200

    Structured basic page layout

---
+++ app/javascript/components/layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const layout = () => (
+  <>
+    <header>Hello Rails React</header>
+    <main>
+      <Outlet />
+    </main>
+    <footer>&copy; All rights reserved</footer>
+  </>
+);
+
+export default layout;
