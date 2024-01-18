3b376cbb442101ac71fe22e253834c9788ec5619
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-18 02:13:52 +0200

    Added babel-loader module

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
-const path    = require("path")
-const webpack = require("webpack")
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   entry: {
-    application: "./app/javascript/application.js"
+    application: './app/javascript/application.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
   },
   output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    chunkFormat: "module",
-    path: path.resolve(__dirname, "app/assets/builds"),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map',
+    chunkFormat: 'module',
+    path: path.resolve(__dirname, 'app/assets/builds'),
   },
   plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
+  ],
+};
