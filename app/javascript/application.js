5b7b634cebd3cfa04e0d2882d2350884b597b594
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2024-01-18 05:13:24 +0200

    Implemented redux store & react router

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Store from './store';
+import Layout from './components/layout';
+import Greeting from './components/greeting';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+const App = () => (
+  <Provider store={Store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Greeting />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
 createRoot(document.getElementById('root')).render(<App />);
